6379508e771abd940291179722f348eea39c4faa
Greyson Elkins <greysonelkins@gmail.com>
2020-09-02 09:43:10 -0600

    Create table for songs

--- src/components/SongTable/SongTable.js
+++ src/components/SongTable/SongTable.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import './SongTable.css'
 
 const SongTable = ({ allSongs }) => {
 
-  const galleryWithSongs = allSongs.map(song => {
+  const tableRowsWithSongs = allSongs.map(song => {
     // return a movieCard
   })
 
-  return (<>
-    </>
+  return (
+    <table>
+      <thead>
+        <tr> Songs </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Song</td>
+          <td>Artist</td>
+          <td>Listen</td>
+        </tr>
+        {tableRowsWithSongs}
+      </tbody>
+    </table>
   )
 }
 
