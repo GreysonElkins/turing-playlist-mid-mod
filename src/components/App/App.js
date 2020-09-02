86472d4e5ce5cf5e91090d3ea1eef0f34d378455
Greyson Elkins <greysonelkins@gmail.com>
2020-09-02 09:26:28 -0600

    Fetch songs into App state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import ApiHelper from '../../ApiHelper/ApiHelper'
 import SongController from '../SongController/SongController';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      allSongs: [],     
       songQueue: []
     }
   }
 
+  componentDidMount() {
+    ApiHelper.getSongs()
+      .then((allFetchedSongs) => this.setState({allSongs: allFetchedSongs}))
+  }
 
   render() {
     return (
