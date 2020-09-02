6b9a1fe9ac19878b725a2c71569776451a49d976
Greyson Elkins <greysonelkins@gmail.com>
2020-09-02 10:26:11 -0600

    Create SongSubmit test file

---
+++ src/components/SongSubmit/SongSubmit.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { screen, render } from '@testing-library/react'
+import "@testing-library/jest-dom";
+import SongSubmit from './SongSubmit
+
+describe('SongSubmit: Adding songs to the playlist', () => {
+
+  beforeEach(() => {
+    render(<SongSubmit />)
+  })
+
+  it('should render three inputs to the page' () => {
+    
+  })
+})
