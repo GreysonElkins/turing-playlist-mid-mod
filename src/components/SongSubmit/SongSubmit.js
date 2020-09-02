658e157aff917f079cc0f4fbd018068b4d94dfb6
Greyson Elkins <greysonelkins@gmail.com>
2020-09-02 09:59:33 -0600

    Control submission form with state

--- src/components/SongSubmit/SongSubmit.js
+++ src/components/SongSubmit/SongSubmit.js
@@ -10,9 +10,31 @@ class SongSubmit extends Component {
     }
   }
 
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
   render() {
     return (
-      ''
+      <>
+      <h2>Add a song to the list!</h2>
+        <input 
+          name="suggestedSong"
+          placeholder="Song title"
+          onChange={this.handleChange}
+          />
+        <input 
+          name="suggestedArtist"
+          placeholder="Artist"
+          onChange={this.handleChange}
+          />
+        <input 
+          name="suggestedListen"
+          placeholder="Link to Listen"
+          onChange={this.handleChange}
+        />
+        <button>Submit</button>
+      </>
     )
   }
 }
